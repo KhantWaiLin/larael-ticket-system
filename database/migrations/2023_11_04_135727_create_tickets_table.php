aa0fcc444b50f024163b3ceefcb8172f2987573a
KhantWaiLin <khantkhant.cm@gmail.com>
2023-11-04 20:57:46 +0630

    learn how to make migration and how to build schema with enums and foreignKey

--- database/migrations/2023_11_04_135727_create_tickets_table.php
+++ database/migrations/2023_11_04_135727_create_tickets_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\TicketStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('status')->default(TicketStatus::OPEN->value);
+            $table->string('attachment')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('status_changed_by_id')->constrained('users');
             $table->timestamps();
         });
     }
