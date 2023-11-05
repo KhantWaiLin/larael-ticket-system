9ab18c70789592816e352d3be248823263c0eb0c
KhantWaiLin <khantkhant.cm@gmail.com>
2023-11-05 10:32:09 +0630

    adding create ticket ui form

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,8 +18,15 @@
                 </div>
             </div>
 
+
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="hidden sm:flex sm:items-center sm:ml-6 flex gap-5">
+                <!-- create ticket button -->
+                <div class="flex flex-col justify-center">
+                    <a href="{{route('ticket.create')}}" class="bg-indigo-500 rounded-lg hover:bg-indigo-600  p-2  text-white">
+                        Support ticket
+                    </a>
+                </div>
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -42,8 +49,7 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -88,8 +94,7 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
--- resources/views/ticket/create.blade.php
+++ resources/views/ticket/create.blade.php
@@ -1,3 +1,27 @@
 <x-app-layout>
-    Hello this is the create ticket page
+    <div class="w-screen flex flex-col justify-center items-center" style="height: calc(100vh - 4em)">
+        <h1 class="flex justify-center items-center">Create a new support ticket</h1>
+        <form method="post" action="{{route('ticket.store')}}" class="w-[20em] mt-2 bg-gray-200 p-5 rounded-lg">
+            @csrf
+            @method('post')
+            <div>
+                <x-input-label for="title" value="Title" />
+                <x-text-input id="title" name="title" type="text" value="" required autofocus autocomplete="title" class=" mt-1 mb-3 block w-full resize-none rounded-lg border-0 focus:border-5 border-indigo-400" />
+                <x-input-error class="mt-2" :messages="$errors->get('title')" />
+            </div>
+            <div>
+                <x-input-label for="description" value="Description" />
+                <textarea name="description" id="description" class="w-full mt-1 mb-3 resize-none rounded-lg border-0 focus:border-5 border-indigo-400">
+
+                </textarea>
+            </div>
+            <div>
+                <x-input-label for="attachment" value="Attachment" class="mb-3" />
+                <input type="file" name="attachment" class="w-full" />
+            </div>
+            <div class="mt-5 w-full flex justify-end items-end">
+                <x-primary-button>Create</x-primary-button>
+            </div>
+        </form>
+    </div>
 </x-app-layout>
